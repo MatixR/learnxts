7bfa7d484eb464e290c2b53532373b5bc981c4ec
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2017-10-10 22:17:04 +0200

    Update 01_Examples_Scripts.R

--- 01_Examples_Scripts.R
+++ 01_Examples_Scripts.R
@@ -65,3 +65,14 @@ flights_xts$pct_divert <- (flights_xts$divert_flights/flights_xts$total_flights)
 # Use plot.zoo() to view all three trends over time
 plot.zoo(x = flights_xts[ , c("pct_delay", "pct_cancel", "pct_divert")])
 
+# Save your xts object to rds file using saveRDS
+saveRDS(object = flights_xts, file = "flights_xts.rds")
+
+# Read your flights_xts data from the rds file
+flights_xts2 <- readRDS("flights_xts.rds")
+
+# Check the class of your new flights_xts2 object
+class(flights_xts2)
+
+# Examine the first five rows of your new flights_xts2 object
+head(flights_xts2, 5)
