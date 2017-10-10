3fbe7150daebda70f038c34b10491dee1b2b6d0e
Vladimir Zhbanko <vz.home.experiments@gmail.com>
2017-10-10 21:39:05 +0200

    Update 01_Examples_Scripts.R

--- 01_Examples_Scripts.R
+++ 01_Examples_Scripts.R
@@ -27,3 +27,12 @@ class(flights_xts)
 
 # Examine the first five lines of flights_xts
 head(flights_xts, 5)
+
+# Identify the periodicity of flights_xts
+periodicity(flights_xts)
+
+# Identify the number of periods in flights_xts
+nmonths(flights_xts)
+
+# Find data on flights arriving in BOS in June 2014
+flights_xts["2014-06"]
